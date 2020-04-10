139cf4677d02aeb5e278d16eed12f5d693c4bcba
Firzan Armani <mrfirzanarmani@gmail.com>
2020-04-10 17:57:59 +0800

    Replace menuBar with Notably logo name

--- src/main/java/com/notably/view/MainWindow.java
+++ src/main/java/com/notably/view/MainWindow.java
@@ -11,12 +11,8 @@ import com.notably.model.Model;
 import com.notably.view.blockcontent.BlockContent;
 
 import javafx.beans.Observable;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextInputControl;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -45,7 +41,7 @@ public class MainWindow extends ViewPart<Stage> {
     private StackPane commandBoxPlaceholder;
 
     @FXML
-    private MenuItem helpMenuItem;
+    private Label notablyLogo;
 
     @FXML
     private StackPane sideBarPlaceholder;
@@ -55,6 +51,9 @@ public class MainWindow extends ViewPart<Stage> {
 
     @FXML
     private StackPane blockContentPlaceholder;
+
+    @FXML
+    private VBox sideBar;
 
     @FXML
     private VBox suggestionsWindow;
@@ -70,46 +69,11 @@ public class MainWindow extends ViewPart<Stage> {
         // Configure the VIEW
         setWindowDefaultSize(logic.getGuiSettings());
 
-        setAccelerators();
-
         initializeHelpWindow(model);
     }
 
     public Stage getPrimaryStage() {
         return primaryStage;
-    }
-
-    private void setAccelerators() {
-        setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
-    }
-
-    /**
-     * Sets the accelerator of a MenuItem.
-     * @param keyCombination the KeyCombination value of the accelerator
-     */
-    private void setAccelerator(MenuItem menuItem, KeyCombination keyCombination) {
-        menuItem.setAccelerator(keyCombination);
-
-        /*
-         * TODO: the code below can be removed once the bug reported here
-         * https://bugs.openjdk.java.net/browse/JDK-8131666
-         * is fixed in later version of SDK.
-         *
-         * According to the bug report, TextInputControl (TextField, TextArea) will
-         * consume function-key events. Because CommandBox contains a TextField, some accelerators (e.g F1)
-         * will not work when the focus is in them because the key event is consumed by
-         * the TextInputControl(s).
-         *
-         * For now, we add following event filter to capture such key events and open
-         * help window purposely so to support accelerators even when focus is
-         * in CommandBox.
-         */
-        getRoot().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
-            if (event.getTarget() instanceof TextInputControl && keyCombination.match(event)) {
-                menuItem.getOnAction().handle(new ActionEvent());
-                event.consume();
-            }
-        });
     }
 
     /**
@@ -141,6 +105,7 @@ public class MainWindow extends ViewPart<Stage> {
             primaryStage.setX(guiSettings.getWindowCoordinates().getX());
             primaryStage.setY(guiSettings.getWindowCoordinates().getY());
         }
+        sideBar.setPrefWidth(guiSettings.getWindowWidth() * 0.25);
     }
 
     /**
